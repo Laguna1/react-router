2aeebedc1370711cb32622ca7bd13aa1af5d5fe5
Laguna1 <lagunazoomarket@gmail.com>
2021-08-01 23:34:13 +0300

    Add navigation links to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,17 @@ function Header() {
   return (
     <div className="header">
       <h1>React router</h1>
+      <ul className="nav-links">
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/about">About</a>
+        </li>
+        <li>
+            <a href="/profile">Profile</a>
+        </li>
+      </ul>
     </div>
   );
 }
