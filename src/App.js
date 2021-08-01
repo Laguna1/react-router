80a360c0433dbb0551ced5b8f59c9cca05755ff2
Laguna1 <lagunazoomarket@gmail.com>
2021-08-01 23:15:44 +0300

    Add 3 pages and routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import About from './Pages/About';
+import Home from './Pages/Home';
+import Profile from './Pages/Profile'; 
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header/>
-        <h1>React router</h1>
       </div>
+      <Route path="/" component={Home} exact />
+      <Route path="/about" component={About} />
+      <Route path="/profile" component={Profile} />
     </BrowserRouter>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import './Header.css';
 function Header() {
   return (
     <div className="header">
-      <h1>Header</h1>
+      <h1>React router</h1>
     </div>
   );
 }
