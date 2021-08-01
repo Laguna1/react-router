df8b055c18b3c0bf7029325977f058025c314b7d
Laguna1 <lagunazoomarket@gmail.com>
2021-08-02 02:05:41 +0300

    Implement redirect to home page when user is not login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import About from './Pages/About';
@@ -20,7 +20,9 @@ const [login, setLogin] = useState(false);
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/about" component={About} />
-        <Route path="/profile" component={Profile} />
+        <Route path="/profile">
+          {login? <Profile /> : <Redirect to="/" />}
+        </Route>
         <Route path="/post/:id" component={Post} />
         <Route component={NotFound} />
       </Switch>
