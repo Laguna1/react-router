29f3ccaaf589693b3da16bae7203f3b656e9554b
Laguna1 <lagunazoomarket@gmail.com>
2021-08-01 23:18:55 +0300

    Add switch to routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import About from './Pages/About';
@@ -11,9 +11,11 @@ function App() {
       <div className="App">
         <Header/>
       </div>
-      <Route path="/" component={Home} exact />
-      <Route path="/about" component={About} />
-      <Route path="/profile" component={Profile} />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/profile" component={Profile} />
+      </Switch>
     </BrowserRouter>
   );
 }
