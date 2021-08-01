3b8b3537df05dfba607af8659b70e9905aa9218d
Laguna1 <lagunazoomarket@gmail.com>
2021-08-01 22:56:47 +0300

    Install react-router-dom dependency

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <h1>React router</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <h1>React router</h1>
+      </div>
+    </BrowserRouter>
   );
 }
 
